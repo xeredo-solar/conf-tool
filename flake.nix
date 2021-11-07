0b851c0285eb50884ff3170eb876c70a390ffa95
Maciej Krüger <mkg20001@gmail.com>
2021-11-07 12:21:27 +0100

    chore: upgrade deps

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Tool to manage the nixOS config";
 
-  inputs.solar.url = "github:ssd-solar/solaros-nix/flake";
+  inputs.solar.url = "github:ssd-solar/solaros-nix/master";
 
   outputs = { self, nixpkgs, solar }: {
     defaultPackage = solar.lib.forAllSystems({ pkgs, ... }: pkgs.callPackage ./package.nix { });
